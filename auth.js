394512aec35f72b40164e121ac6b0ca7ccbe6567
Chris Santoso <csantoso97@gmail.com>
2018-05-10 01:59:35 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 module.exports = {  
     CLIENT_ID: '<client_id>', 
-    CLIENT_SECRET: '<client_secret>,  
+    CLIENT_SECRET: '<client_secret>',  
 
     SSL_KEY: fs.readFileSync('certs/domain.key', 'utf8'),  
     SSL_CERT: fs.readFileSync('certs/domain.crt', 'utf8'),   
